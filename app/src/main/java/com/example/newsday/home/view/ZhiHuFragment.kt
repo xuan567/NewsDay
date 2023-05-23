cf78aea90fd6a6bea1ab82d06ab86a86c165dcd3
xuan567 <572351128@qq.com>
2023-05-23 12:03:19 +0800

    知乎热榜换成固定数据

--- app/src/main/java/com/example/newsday/home/view/ZhiHuFragment.kt
+++ app/src/main/java/com/example/newsday/home/view/ZhiHuFragment.kt
@@ -32,6 +32,7 @@ class ZhiHuFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var binding: FragmentZhiHuBinding
+    private val zhihuList = ArrayList<HotItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,32 +45,162 @@ class ZhiHuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentZhiHuBinding.inflate(inflater, container, false)
         val hotListViewModel = ViewModelProvider(this)[HotListViewModel::class.java]
         hotListViewModel.getHotList("zhihu")
         hotListViewModel.initCommendDetailDateBaseHelper(requireContext())
-        hotListViewModel.hotListLiveData.observe(requireActivity()){
-            if (it == null) {
-                Log.d("Hot", "zhihu: it==null")
-                return@observe
+//        hotListViewModel.hotListLiveData.observe(requireActivity()) {
+//            if (it == null) {
+//                Log.d("Hot", "zhihu: it==null")
+//                return@observe
+//            }
+//            val layoutManager = LinearLayoutManager(context)
+//            val adapter = ZhiHuAdapter(it)
+//            binding.zhihuRecycler.layoutManager = layoutManager
+//            binding.zhihuRecycler.adapter = adapter
+//            binding.zhihuRecycler.setItemViewCacheSize(50)
+//            adapter.setOnItemClickListener(object : ZhiHuAdapter.OnItemClickListener {
+//                override fun onItemClick(view: View?, position: Int) {
+//                    val intent = Intent(Intent.ACTION_VIEW)
+//                    val string = it[position].link
+//                    intent.data = Uri.parse(string)
+//                    startActivity(intent)
+//                }
+//            })
+//        }
+        initListData()
+        val layoutManager = LinearLayoutManager(context)
+        val adapter = ZhiHuAdapter(zhihuList)
+        binding.zhihuRecycler.layoutManager = layoutManager
+        binding.zhihuRecycler.adapter = adapter
+        binding.zhihuRecycler.setItemViewCacheSize(50)
+        adapter.setOnItemClickListener(object : ZhiHuAdapter.OnItemClickListener {
+            override fun onItemClick(view: View?, position: Int) {
+                val intent = Intent(Intent.ACTION_VIEW)
+                val string = zhihuList[position].link
+                intent.data = Uri.parse(string)
+                startActivity(intent)
             }
-            val layoutManager = LinearLayoutManager(context)
-            val adapter = ZhiHuAdapter(it)
-            binding.zhihuRecycler.layoutManager = layoutManager
-            binding.zhihuRecycler.adapter = adapter
-            binding.zhihuRecycler.setItemViewCacheSize(50)
-            adapter.setOnItemClickListener(object : ZhiHuAdapter.OnItemClickListener {
-                override fun onItemClick(view: View?, position: Int) {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    val string = it[position].link
-                    intent.data = Uri.parse(string)
-                    startActivity(intent)
-                }
-            })
-        }
+        })
+
         return binding.root
 
+    }
+
+    private fun initListData() {
+        zhihuList.add(
+            HotItem(
+                1,
+                "https://www.zhihu.com/question/602362308",
+                "653 万热度",
+                "2023 年退休人员基本养老金上调 3.8% ，将带来哪些影响？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                2,
+                "https://www.zhihu.com/question/602439506",
+                "1229 万热度",
+                "俄方称一支乌军部队攻入俄罗斯境内，具体情况如何？将对俄乌局势产生哪些影响？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                3,
+                "https://www.zhihu.com/question/547031865",
+                "429 万热度",
+                "为什么做梦时大脑生成的梦境世界如此宏大且真实，而清醒时大脑内存却这么低？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                4,
+                "https://www.zhihu.com/question/601942509",
+                "294 万热度",
+                "刘若英演唱会试点强制实名入场，开启临时退票通道，这一措施会得到推广吗？「强实名」购票入场实施为什么难"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                5,
+                "https://www.zhihu.com/question/599884016",
+                "287 万热度",
+                "男朋友给正在同居的我们列了一个预算表，所有东西AA，我看完很不舒服，是我太自私吗？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                6,
+                "https://www.zhihu.com/question/601857583",
+                "116 万热度",
+                "新文章提出取消作者姓名排序，人人都是一作，你赞同这种署名方案吗？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                7,
+                "https://www.zhihu.com/question/602087055",
+                "111 万热度",
+                "贫铀弹，是为了其硬度还是仅仅为了辐射杀伤？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                8,
+                "https://www.zhihu.com/question/602229772",
+                "106 万热度",
+                "孕妇遭陌生男子猥亵，报警时被打致耳聋，嫌疑人已被刑拘，如何看待此事？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                9,
+                "https://www.zhihu.com/question/602504870",
+                "97 万热度",
+                "尤文图斯案重审结果官宣，本赛季被扣除 10 分，尤文降至意甲第七，如何看待这个处罚结果？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                10,
+                "https://www.zhihu.com/question/602332945",
+                "89 万热度",
+                "钟南山称 6 月底或是今年第二波疫情高峰，上班族如何做好防护工作？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                11,
+                "https://www.zhihu.com/question/602330182",
+                "82 万热度",
+                "东部决赛第三场，热火 128 比 102 战胜凯尔特人，三比零领先，如何评价这场比赛？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                12,
+                "https://www.zhihu.com/question/601843353",
+                "80 万热度",
+                "网易子公司正在开发《战锤》新作，你对此游戏有哪些期待?"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                13,
+                "https://www.zhihu.com/question/601725007",
+                "76 万热度",
+                "神舟十六号船箭组合体转运至发射区，计划近日择机实施发射，有何期待和祝福？"
+            )
+        )
+        zhihuList.add(
+            HotItem(
+                14,
+                "https://www.zhihu.com/question/600353143",
+                "57 万热度",
+                "同事不带饭，天天蹭我的，社恐人应该怎么拒绝？"
+            )
+        )
     }
 
     class ZhiHuAdapter(val zhihuList: List<HotItem>) :
